171af07b7f3ea0cbd6c4de1b72feb994e8115baa
fel-canto <fel.canto@duocuc.cl>
2022-05-03 23:00:26 -0400

    formulario sin validacion
    formulario para añadir productos(ADMIN)

---
+++ JS/validacionAgregarProd.js
@@ -0,0 +1,13 @@
+const formulario = document.getElementById("form");
+const inputs = document.querySelectorAll('#form input');
+var nombre = document.getElementById("nomProd");
+var descripcion = document.getElementById("descProd");
+var mensaje = document.getElementById("mensaje");
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+});
+
+
+
