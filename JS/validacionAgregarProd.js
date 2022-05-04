54bbd11d26507431c2c1eec3ce05ff0eba033dd2
fel-canto <fel.canto@duocuc.cl>
2022-05-04 01:12:02 -0400

    formulario con validacion no muestra mensajes

--- JS/validacionAgregarProd.js
+++ JS/validacionAgregarProd.js
@@ -1,13 +1,44 @@
-const formulario = document.getElementById("form");
-const inputs = document.querySelectorAll('#form input');
-var nombre = document.getElementById("nomProd");
-var descripcion = document.getElementById("descProd");
-var mensaje = document.getElementById("mensaje");
+const nombre = document.getElementById("nomProd");
+const descripcion = document.getElementById("descProd");
+const precio = document.getElementById("precio");
+const cantidad = document.getElementById("cantidad");
+const form = document.getElementById("form");
+const listInputs = document.querySelectorAll(".form-input");
 
-formulario.addEventListener("submit", (e) => {
-    e.preventDefault();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let condicion = validacionForm();
+  if (condicion) {
+    enviarFormulario();
+  }
 
+function validacionForm() {
+  form.lastElementChild.innerHTML = "";
+  let condicion = true;
+  listInputs.forEach((element) => {
+    element.lastElementChild.innerHTML = "";
+  });
+
+  if (nombre.value.length < 7 || nombre.value.trim() == "") {
+    mostrarMensajeError("nomProd", "Nombre no valido*");
+    condicion = false;
+  }
+  if (descripcion.value.length < 15 || descripcion.value.trim() == "") {
+    mostrarMensajeError("descProd", "Descripción muy corta");
+    condicion = false;
+  }
+  return condicion;
+}
 });
 
+function mostrarMensajeError(claseInput, mensaje) {
+  let elemento = document.querySelector(`.${claseInput}`);
+  elemento.lastElementChild.innerHTML = mensaje;
+}
+
+function enviarFormulario() {
+  form.reset();
+  form.lastElementChild.innerHTML = "Producto agregado";
+}
 
 
